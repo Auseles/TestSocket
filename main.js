52a66825538ae7e25409f1ef6aaf05d4d58a3b18
Auseles <117265371+Auseles@users.noreply.github.com>
2025-02-24 13:03:18 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 
+let url = window.location.href;
+let paramsStr = url.split('?')[1];
+let params = new URLSearchParams(paramsStr);
+
+for (let pair of params.entries()) 
+{
+    let name = pair[0];
+    let value = pair[1];
+
+    document.write(name + "=" + value + '; ');
+}
+
 wss.on('connection', function connection(ws) {
 
     console.log('Client connected');
